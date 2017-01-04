9c9ea5727eb38795ea0cc9ee70df3f75e9f3200a
noorsil.karedia@mobilelive.ca <noorsil.karedia@mobilelive.ca>
2017-01-04 02:08:57 -0500

    added actionview

--- src/pages/tg-menu/tg-menu.ts
+++ src/pages/tg-menu/tg-menu.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, ModalController, ViewController } from 'ionic-angular';
+
 
 @Component({
   selector: 'page-tg-menu',
@@ -6,10 +8,63 @@ import { Component } from '@angular/core';
 })
 export class TgMenuPage {
 
-  constructor() {}
+  constructor(private actionCtrl: ActionSheetController, private modalCtrl: ModalController) {
+
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TgMenuPage');
   }
+
+  presentActionSheet() {
+    let actionSheet = this.actionCtrl.create({
+      title: 'Modify your album',
+      buttons: [
+        {
+          text: 'Destructive',
+          role: 'destructive',
+          handler: () => {
+            console.log('Destructive clicked');
+          }
+        }, {
+          text: 'Archive',
+          handler: () => {
+            console.log('Archive clicked');
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  presentModal() {
+    let modal = this.modalCtrl.create(AddList);
+    modal.present();
+  }
+
+}
+
+
+
+@Component({
+  template: `
+    Hello World
+  `
+})
+class AddList {
+  constructor(private viewCtrl: ViewController) {
+
+  }
+
+  dissmiss() {
+    this.viewCtrl.dismiss();
+  }
+
 
 }
