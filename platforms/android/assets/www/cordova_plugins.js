870767c805415327ae9cb4efc36faebc6029a1e6
noorsil.karedia@mobilelive.ca <noorsil.karedia@mobilelive.ca>
2017-01-04 00:26:43 -0500

    adding add form

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,36 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-statusbar.statusbar",
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-headercolor.HeaderColor",
+        "file": "plugins/cordova-plugin-headercolor/www/HeaderColor.js",
+        "pluginId": "cordova-plugin-headercolor",
+        "clobbers": [
+            "cordova.plugins.headerColor"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-statusbar": "2.2.1",
+    "cordova-plugin-splashscreen": "4.0.1",
+    "cordova-plugin-headercolor": "1.0"
+};
+// BOTTOM OF METADATA
+});
